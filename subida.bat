b7596117d620a9a16e442ffe8bcc1da868b8c305
garciaantonio20k <antonio.garcia.marin.sspa@juntadeandalucia.es>
2023-12-27 09:09:52 +0100

    cambios

---
+++ subida.bat
@@ -0,0 +1,18 @@
+copy comite-de-honor_798772499_131092.html webfinal\es\comite-de-honor_798772499_131092.html
+copy comite-tecnico_798772500_131093.html webfinal\es\comite-tecnico_798772500_131093.html
+copy contacto_798772498_131091.html webfinal\es\contacto_798772498_131091.html
+copy inicio_798772332_130763.html webfinal\es\inicio_798772332_130763.html
+copy politica-privacidad_798772645_131381.html webfinal\es\politica-privacidad_798772645_131381.html
+copy programa_798772614_131312.html webfinal\es\programa_798772614_131312.html
+copy programa-paralelo_798772644_131374.html webfinal\es\programa-paralelo_798772644_131374.html
+copy terminos-y-condiciones_798772645_131382.html webfinal\es\terminos-y-condiciones_798772645_131382.html
+copy prensa_798772497_131085.html webfinal\es\prensa_798772497_131085.html
+copy contact_798772658_131406.html webfinal\en
+copy home_798772658_131399.html webfinal\en
+copy honour-committee_798772658_131402.html webfinal\en
+copy parallel-programme_798772658_131401.html webfinal\en
+copy privacy-policy_798772658_131404.html webfinal\en
+copy program_798772658_131400.html webfinal\en
+copy technical-committee_798772658_131403.html webfinal\en
+copy terms-and-conditions_798772658_131405.html webfinal\en
+pause
